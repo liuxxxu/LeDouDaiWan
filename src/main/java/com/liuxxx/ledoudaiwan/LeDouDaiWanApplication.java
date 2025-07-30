f0d33bd3b59e3f5c25beac01aee7d2788c78fc7b
liuxxx <liuxucn@outlook.com>
2025-07-30 08:46:46 +0800

    项目初始化

---
+++ src/main/java/com/liuxxx/ledoudaiwan/LeDouDaiWanApplication.java
@@ -0,0 +1,13 @@
+package com.liuxxx.ledoudaiwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeDouDaiWanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeDouDaiWanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liuxxx/ledoudaiwan/LeDouDaiWanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liuxxx.ledoudaiwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeDouDaiWanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
